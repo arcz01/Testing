fb51dea72a4af0f10dbb3334fef7894e5ee1cdcc
Juan Miguel Garcia <0nameless5@gmail.com>
2018-08-31 07:45:55 +0800

    new

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,11 @@ import { MyApp } from './app.component';
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
+
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-import { SettingPage } from '../pages/setting/setting';
 
 @NgModule({
   declarations: [
@@ -19,7 +19,6 @@ import { SettingPage } from '../pages/setting/setting';
     ContactPage,
     HomePage,
     TabsPage,
-    SettingPage
   ],
   imports: [
     BrowserModule,
@@ -32,7 +31,6 @@ import { SettingPage } from '../pages/setting/setting';
     ContactPage,
     HomePage,
     TabsPage,
-    SettingPage
   ],
   providers: [
     StatusBar,
